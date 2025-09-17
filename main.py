66d940c75f2fb5067d8dc36e3b29a64bd39f17de
Anromit <romitsina.a.r@gmail.com>
2025-09-17 16:55:28 +0300

    init: scaffold venv, .env sample, deps

---
+++ main.py
@@ -0,0 +1,28 @@
+import os
+from dotenv import load_dotenv
+import telebot
+load_dotenv()
+TOKEN = os.getenv("TOKEN")
+if not TOKEN:
+ raise RuntimeError("В .env нет TOKEN")
+bot = telebot.TeleBot(TOKEN)
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+ bot.reply_to(message, "Привет! Я твой первый бот! Напиши /help")
+ 
+ 
+@bot.message_handler(commands=['help'])
+def help_cmd(message):
+ bot.reply_to(message, "/start - начать\n/help - помощь\n/about - что я умею")
+ 
+ 
+@bot.message_handler(commands=['about'])
+def about(message):
+ bot.reply_to(message, "лол")
+
+ 
+if __name__ == "__main__":
+ bot.infinity_polling(skip_pending=True)
+
